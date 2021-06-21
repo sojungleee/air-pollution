b391d833c433566d913959d861da0f18398552bb
임유선 <1991167@hansung.ac.kr>
2021-06-22 01:00:14 +0900

    Feat: 추가 lcd print 함수 - close #2

---
+++ lcd.py
@@ -0,0 +1,28 @@
+import drivers
+from time import sleep
+
+# data가 {'co2':0.01, ...} 과 같은 딕셔너리 형태로
+# 구성되어 있다고 가정함
+# 한줄씩 출력
+
+def print_sensor_data(data: dict):
+
+    display = drivers.Lcd()
+
+    try:
+        while True:
+            line_num = 1
+            print("print sensor data...")
+
+            for key, value in data.items():
+                value = str(value)
+                display.lcd_display_string('{0:<16}'.format(key + ":" + value), line_num)
+
+                line_num += 1
+                sleep(1.5)
+
+                if line_num == 3 :
+                    line_num = 1
+
+    except KeyboardInterrupt:
+        display.lcd_clear()
