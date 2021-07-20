5bd42efebdb6eb14e6db01bba2c54cfde7c4e44c
임유선 <1991167@hansung.ac.kr>
2021-07-20 06:32:11 +0100

    Comment: 추가 앞으로 짤 코드 설명

--- collect.py
+++ collect.py
@@ -3,22 +3,28 @@ import time
 import threading
 from collections import defaultdict
 
-def collect(data: defaultdict):
+def collect_air_quality(data: defaultdict):
     pm2_5, pm10 = pm2008.pm2008()
+    # co = co.co()
 
     data["pm2_5"] = pm2_5
     data["pm10"] = pm10
+    # data['co'] = co
 
     # threading.Timer(1,collect(data)).start()
     # time.sleep(1)
 
     print(data)
-    return data
 
 if __name__ == "__main__":
     data = defaultdict()
     # t = threading.Thread(target = collect, args=(data,))
     # t.start()
-    # 스레드 관리는 sql문 update할때 collect()함수 부르는걸
-    # 무한으로 돌려야 할듯
-    collect(data)
+
+    ######### 여기부터 
+    collect_air_quality(data)
+    #insert_raspdb.insert_raspdb_air_quality(id, data["co"], data["pm2_5"], data["pm10"])
+    
+    #collect_gps(gps_data)
+    #insert_raspdb.indsert_gps~~~
+    ######### 여기까지 1초에 한번씩 시행
