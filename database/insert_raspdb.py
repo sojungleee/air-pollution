040b7c50b5bdf30f2dadb73c57766aa8f80c2263
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2021-08-06 23:08:45 +0900

    Feat: DB수정에 따른 DB삽입&조회 코드 수정

--- database/insert_raspdb.py
+++ database/insert_raspdb.py
@@ -1,40 +1,11 @@
 import pymysql
 
-def insert_raspdb_air_quality(id: str, co: float, pm2_5: int, pm10: int,air_index: int) :
-    # connect to database
-    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
-    db = 'mydb', charset='utf8')
-    cur = db.cursor()
-
-    # insert
-    sql = "INSERT INTO air_quality_sensor VALUES (%s, %s, %s, %s, %s)"
-    val = (id, co, pm2_5, pm10, air_index)
-    cur.execute(sql, val)
-
-    db.commit()
-
-    print(cur.rowcount, "air_quality record inserted")
-
-def insert_raspdb_gps(id: str, latitude: int, longtitude: int):
-    #connect to database
-    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
-    db = 'mydb', charset='utf8')
-    cur = db.cursor()
-
-    # insert
-    sql = "INSERT INTO gps VALUES (%s, %s, %s)"
-
-    val = (id, latitude, longtitude)
-    cur.execute(sql, val)
-
-    db.commit()
-
-    print(cur.rowcount, "gps record inserted")
+#
 
 def insert_raspdb_device(id: str, network_condition: bool):
     #connect to database
     db = pymysql.connect(host='localhost', user='root', password='raspberry', 
-    db = 'mydb', charset='utf8')
+    db = 'raspdb', charset='utf8')
     cur = db.cursor()
 
     # insert
@@ -47,4 +18,73 @@ def insert_raspdb_device(id: str, network_condition: bool):
     
     print(cur.rowcount, "device row inserted")
 
+#
 
+def insert_raspdb_gps(geohash: str, latitude: int, longitude: int):
+    #connect to database
+    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
+    db = 'raspdb', charset='utf8')
+    cur = db.cursor()
+
+    # insert
+    sql = "INSERT INTO gps(geohash, latitude, longitude) VALUES (%s, %s, %s)"
+
+    val = (geohash, latitude, longitude)
+    cur.execute(sql, val)
+
+    db.commit()
+
+    print(cur.rowcount, "gps record inserted")
+
+#
+
+def insert_raspdb_air_quality(air_quality_id: str, device_id: str, co: float, pm10: int, pm2_5: int):
+    # connect to database
+    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
+    db = 'raspdb', charset='utf8')
+    cur = db.cursor()
+
+    # insert
+    sql = "INSERT INTO air_quality_sensor VALUES (%s, %s, %s, %s, %s)"
+    val = (air_quality_id, device_id, co, pm10, pm2_5)
+    cur.execute(sql, val)
+
+    db.commit()
+
+    print(cur.rowcount, "air_quality record inserted")
+
+#
+
+def insert_raspdb_sensors(sensor_id: str, air_quality_id: str, geohash: str):
+    #connect to database
+    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
+    db = 'raspdb', charset='utf8')
+    cur = db.cursor()
+
+    # insert
+    sql = "INSERT INTO sensors(sensor_id, air_quality_id, geohash) VALUES (%s, %s, %s)"
+
+    val = (sensor_id, air_quality_id, geohash)
+    cur.execute(sql, val)
+
+    db.commit()
+
+    print(cur.rowcount, "sensors record inserted")
+
+#
+
+def insert_raspdb_geohash(geohash: str, sensor_id: str, address: str):
+    #connect to database
+    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
+    db = 'raspdb', charset='utf8')
+    cur = db.cursor()
+
+    # insert
+    sql = "INSERT INTO geohash VALUES (%s, %s, %s)"
+
+    val = (geohash, sensor_id, address)
+    cur.execute(sql, val)
+
+    db.commit()
+
+    print(cur.rowcount, "geohash record inserted")
