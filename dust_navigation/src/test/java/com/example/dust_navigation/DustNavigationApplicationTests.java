ffa41b2305098a1601c3cd879247f97c09c7770e
임유선 <1991167@hansung.ac.kr>
2021-08-11 17:14:33 +0900

    Feat: geohash 변환 추가

--- dust_navigation/src/test/java/com/example/dust_navigation/DustNavigationApplicationTests.java
+++ dust_navigation/src/test/java/com/example/dust_navigation/DustNavigationApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.dust_navigation;
 
+import ch.hsr.geohash.GeoHash;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,10 @@ class DustNavigationApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    void test () {
+
     }
 
 }
