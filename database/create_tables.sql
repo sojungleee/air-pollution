08016d1bf40a85339741e6a21f52af9c684e1c11
임서영 <seoyoung1113@naver.com>
2021-08-15 15:52:37 +0900

    Update database/create_tables.sql

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -5,18 +5,19 @@ CREATE TABLE device (
     PRIMARY KEY (device_id)
 );
 CREATE TABLE air_quality_sensor (
-    air_quality_id TIMESTAMP,
+    geohash char(7) ,
     device_id char(8),
     co DECIMAL(4,2),
     pm10 INT,
     pm25 INT,
-    PRIMARY KEY (air_quality_id),
+    FOREIGN KEY (geohash) REFERENCES location (geohash),
+    PRIMARY KEY (geohash),
     FOREIGN KEY (device_id) REFERENCES device (device_id)
 );
 CREATE TABLE location (
     geohash char(7),
-    air_quality_id TIMESTAMP,
+    latitude float,
+    longitude float,
     recieve_time TIMESTAMP,
     PRIMARY KEY (geohash),
-    FOREIGN KEY (air_quality_id) REFERENCES air_quality_sensor (air_quality_id)
 );
