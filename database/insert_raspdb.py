5df34758d748788a4866d995e2626ed0660bc7e5
임서영 <seoyoung1113@naver.com>
2021-08-15 15:56:00 +0900

    Update database/insert_raspdb.py

--- database/insert_raspdb.py
+++ database/insert_raspdb.py
@@ -18,15 +18,15 @@ def insert_raspdb_device(device_id: str, network_condition: bool,timestamp: str)
 
 #
 
-def insert_raspdb_air_quality(air_quality_id: str, device_id: str, co: float, pm10: int, pm25: int):
+def insert_raspdb_air_quality(geohash: str, device_id: str, co: float, pm10: int, pm25: int):
     # connect to database
     db = pymysql.connect(host='localhost', user='root', password='raspberry', 
     db = 'raspdb', charset='utf8')
     cur = db.cursor()
 
     # insert
-    sql = "INSERT INTO air_quality_sensor(air_quality_id,device_id,co,pm10,pm25) VALUES (%s, %s, %s, %s, %s)"
-    val = (air_quality_id, device_id, co, pm10, pm25)
+    sql = "INSERT INTO air_quality_sensor(geohash,device_id,co,pm10,pm25) VALUES (%s, %s, %s, %s, %s)"
+    val = (geohash, device_id, co, pm10, pm25)
     cur.execute(sql, val)
 
     db.commit()
@@ -35,15 +35,15 @@ def insert_raspdb_air_quality(air_quality_id: str, device_id: str, co: float, pm
 
 #
 
-def insert_raspdb_location(geohash: str, air_quality_id: str):
+def insert_raspdb_location(geohash: str, latitude: float, longitude: float):
     #connect to database
     db = pymysql.connect(host='localhost', user='root', password='raspberry', 
     db = 'raspdb', charset='utf8')
     cur = db.cursor()
 
     # insert
-    sql = "INSERT INTO geohash(geohash, air_quality_id) VALUES (%s, %s)"
-    val = (geohash, air_quality_id)
+    sql = "INSERT INTO geohash(geohash, latitude, longitude) VALUES (%s, %s, %s)"
+    val = (geohash, latitude, longitude)
     cur.execute(sql, val)
 
     db.commit()
