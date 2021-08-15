848ed7362d058173ac7d1b94a23cfccf3c276d93
임서영 <seoyoung1113@naver.com>
2021-08-15 15:57:16 +0900

    Update database/insert_rds.py

--- database/insert_rds.py
+++ database/insert_rds.py
@@ -1,13 +1,13 @@
 import pymysql
 
-def insert_rds_air_quality(air_quality_id: str, device_id: str, co: float, pm10: int, pm25: int):
+def insert_rds_air_quality(geohash: str, device_id: str, co: float, pm10: int, pm25: int):
     #connect to database
     db = pymysql.connect(host='hanium.c1hdrrzsdvm2.ap-northeast-2.rds.amazonaws.com',user= 'admin',password='raspberry',db='raspdb',charset='utf8')
     cur = db.cursor()
 
     #insert
-    sql = "INSERT INTO air_quality_sensor(air_quality_id,device_id,co,pm10,pm25) VALUES (%s, %s, %s, %s, %s)"
-    val = (air_quality_id, device_id, co, pm10, pm25)
+    sql = "INSERT INTO air_quality_sensor(geohash,device_id,co,pm10,pm25) VALUES (%s, %s, %s, %s, %s)"
+    val = (geohash, device_id, co, pm10, pm25)
     cur.execute(sql,val)
 
     db.commit()
@@ -29,14 +29,14 @@ def insert_rds_device(device_id: str, network_condition: bool):
     print(cur.rowcount, "device record inserted")
 #
 
-def insert_raspdb_location(geohash: str, air_quality_id: str):
+def insert_raspdb_location(geohash: str,  latitude: float, longitude: float):
     #connect to database
     db = pymysql.connect(host='hanium.c1hdrrzsdvm2.ap-northeast-2.rds.amazonaws.com',user= 'admin',password='raspberry',db='raspdb',charset='utf8')
     cur = db.cursor()
 
     # insert
-    sql = "INSERT INTO geohash(geohash, air_quality_id) VALUES (%s, %s)"
-    val = (geohash, air_quality_id)
+    sql = "INSERT INTO geohash(geohash, latitude, longitude) VALUES (%s, %s, %s)"
+    val = (geohash, latitude, longitude)
     cur.execute(sql, val)
 
     db.commit()
