6fe4c651a6d06018c7d7cd4584bdc476c4d2e0ff
임유선 <1991167@hansung.ac.kr>
2021-08-17 09:30:17 +0100

    Feat: 센서 평균값 도출 #27

---
+++ get_avg.py
@@ -0,0 +1,16 @@
+import pymysql
+
+def get_avg():
+    #connect to database
+    db = pymysql.connect(host='localhost', user='root', password='raspberry', 
+    db = 'raspdb', charset='utf8')
+    cur = db.cursor()
+
+    # insert
+    sql = "SELECT geohash, MAX(receive_time), AVG(co), AVG(pm10), AVG(pm25) FROM air_quality_sensor GROUP BY (geohash)"
+    cur.execute(sql)
+    row = cur.fetchall()
+
+    for r in row: print(r)
+
+get_avg()
