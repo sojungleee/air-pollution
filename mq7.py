de26f38711e4c2afd2b16220a7bd0c8b1708d13c
gimsein <continue.deve@gmail.com>
2021-08-19 07:36:10 +0900

    Feat:추가 MCP3008.py

--- mq7.py
+++ mq7.py
@@ -1,20 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+from MCP3008 import MCP3008
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 GPIO.setup(27, GPIO.OUT)
- 
-try:
-    while True:
-        if GPIO.input(18):
-         print("Pas de detection")
-         time.sleep(0.2)
-        if GPIO.input(18)!=1:
-         print("Detection de GAS")
-         GPIO.output(27, False)
-         time.sleep(0.1)
-         GPIO.output(27, True)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+def mq7():
+
+    try:
+        while True:
+            if GPIO.input(18):
+                print("Pas de detection")
+                time.sleep(0.2)
+            if GPIO.input(18)!=1:
+                print("Detection de GAS")
+                GPIO.output(27, False)
+                time.sleep(0.1)
+                GPIO.output(27, True)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
