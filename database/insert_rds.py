ce2a31fb8fdae3677703ff31aa3609182177c896
임서영 <seoyoung1113@naver.com>
2021-08-18 14:08:32 +0900

    Update database/insert_rds.py

--- database/insert_rds.py
+++ database/insert_rds.py
@@ -1,12 +1,12 @@
 import pymysql
 
-def insert_rds_air_quality(geohash: str, device_id: str, co: float, pm10: int, pm25: int):
+def insert_rds_air_quality(geohash: str, device_id: str, co: float, pm10: int, pm25: int,timestamp: str):
     #connect to database
     db = pymysql.connect(host='hanium.c1hdrrzsdvm2.ap-northeast-2.rds.amazonaws.com',user= 'admin',password='raspberry',db='raspdb',charset='utf8')
     cur = db.cursor()
 
     #insert
-    sql = "INSERT INTO air_quality_sensor(geohash,device_id,co,pm10,pm25) VALUES (%s, %s, %s, %s, %s)"
+    sql = "INSERT INTO air_quality_sensor(geohash,device_id,co,pm10,pm25,receive_time) VALUES (%s, %s, %s, %s, %s,, %s)"
     val = (geohash, device_id, co, pm10, pm25)
     cur.execute(sql,val)
 
