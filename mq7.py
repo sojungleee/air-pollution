a58aa59000e427da2addd203feaec41c8c74d7d7
gimsein <continue.deve@gmail.com>
2021-08-19 20:25:50 +0900

    FEAT:추가  mq7.js 구현됨, Fix:수정 MCP3008.py 삭제(필요없어짐)

--- mq7.py
+++ mq7.py
@@ -1,23 +1,21 @@
-import RPi.GPIO as GPIO
+import spidev
 import time
-from MCP3008 import MCP3008
- 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN)
-GPIO.setup(27, GPIO.OUT)
 
-def mq7():
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz=500000
 
-    try:
-        while True:
-            if GPIO.input(18):
-                print("Pas de detection")
-                time.sleep(0.2)
-            if GPIO.input(18)!=1:
-                print("Detection de GAS")
-                GPIO.output(27, False)
-                time.sleep(0.1)
-                GPIO.output(27, True)
-
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+def read_spi_adc(adcChannel):
+    adcValue = 0
+    buff = spi.xfer2([1, (8+adcChannel)<<4, 0])
+    adcValue = ((buff[1]&3)<<8)+buff[2]
+    return adcValue
+    
+try :
+    while True :
+        adcChannel = 0
+        adcValue = read_spi_adc(adcChannel)
+        print("%d"%adcValue)
+        time.sleep(10)
+except KeyboardInterrupt:
+    spi.close()
