1ab276c855b85cd6df7005f4cf57ba402e057fa1
임유선 <1991167@hansung.ac.kr>
2021-08-25 00:21:10 +0900

    Feat: 정렬된 get api 함수 추가

--- dust_navigation/src/main/java/com/example/dust_navigation/controller/Controller.java
+++ dust_navigation/src/main/java/com/example/dust_navigation/controller/Controller.java
@@ -2,8 +2,13 @@ package com.example.dust_navigation.controller;
 
 import com.example.dust_navigation.models.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -14,14 +19,41 @@ public class Controller {
 
     // read
     @GetMapping("/api/locations")
-    public List <Location> getLocation() {return locationRepository.findAll(); }
+    public List<Location> getLocation() {
+        return locationRepository.findAll();
+    }
 
     @GetMapping("/api/locations/{geohash}")
     public Location getLocationByGeohash(@PathVariable String geohash) {
         return locationRepository.findById(geohash).orElse(null);
     }
 
-    @GetMapping("/api/airqualitysensors")
-    public List<AirQualitySensor> getAirQualitySensor() {return airQualitySensorRepository.findAll(); }
+    @GetMapping("/api/airqualitysensorsall")
+    public List<AirQualitySensor> getAllAirQualitySensor() {
+        return airQualitySensorRepository.findAll();
+    }
 
+    @GetMapping("/api/airqualitysensors")
+    public List<AirQualitySensor> getAirQualitySensor(@RequestParam String sort, @RequestParam String item, @RequestParam String geohash, @RequestParam(defaultValue = "6") int precision) {
+        List<AirQualitySensor> sortedData = new ArrayList<>();
+        List<AirQualitySensor> result = new ArrayList<>();
+
+        if (sort.equals("asc")) {
+            if (item.equals("co")) sortedData = airQualitySensorRepository.findAllByOrderByCoAsc();
+            else if (item.equals("pm10")) sortedData = airQualitySensorRepository.findAllByOrderByPm10Asc();
+            else if (item.equals("pm25")) sortedData = airQualitySensorRepository.findAllByOrderByPm25Asc();
+            else return null;
+        } else if (sort.equals("desc")) {
+            if (item.equals("co")) sortedData = airQualitySensorRepository.findAllByOrderByCoDesc();
+            else if (item.equals("pm10")) sortedData = airQualitySensorRepository.findAllByOrderByPm10Desc();
+            else if (item.equals("pm25")) sortedData = airQualitySensorRepository.findAllByOrderByPm25Desc();
+            else return null;
+        }
+
+        String subGeohashStr = geohash.substring(0, precision);
+        for (AirQualitySensor sensorData : sortedData) {
+            if (sensorData.getGeohash().substring(0, precision).equals(subGeohashStr)) { result.add(sensorData); }
+        }
+        return result;
+    }
 }
--- dust_navigation/src/main/java/com/example/dust_navigation/models/AirQualitySensorRepository.java
+++ dust_navigation/src/main/java/com/example/dust_navigation/models/AirQualitySensorRepository.java
@@ -2,5 +2,15 @@ package com.example.dust_navigation.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AirQualitySensorRepository extends JpaRepository<AirQualitySensor,String> {
+    List<AirQualitySensor> findAllByOrderByCoAsc();
+    List<AirQualitySensor> findAllByOrderByCoDesc();
+
+    List<AirQualitySensor> findAllByOrderByPm10Asc();
+    List<AirQualitySensor> findAllByOrderByPm10Desc();
+
+    List<AirQualitySensor> findAllByOrderByPm25Asc();
+    List<AirQualitySensor> findAllByOrderByPm25Desc();
 }
