584ea34cbc3c242b29df4f8156ef762b03b95651
임서영 <seoyoung1113@naver.com>
2021-10-01 15:43:34 +0900

    Update loof.py

--- loof.py
+++ loof.py
@@ -2,7 +2,7 @@ import pymysql
 import time
 
 while(True):
-    sleep(5000)
+    time.sleep(5000)
 # connect to database
     db = pymysql.connect(host='localhost',      user='root', password='raspberry', 
     db = 'raspdb', charset='utf8')
